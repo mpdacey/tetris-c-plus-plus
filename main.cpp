252c4d8ab0fa2c4234269ae77cd90e7a1b5e6389
mpdacey <80190692+mpdacey@users.noreply.github.com>
2024-01-05 17:21:07 +1100

    Change rotate index parameter names

--- main.cpp
+++ main.cpp
@@ -3,12 +3,12 @@ using namespace std;
 
 wstring tetromino[7];
 
-int RotatedIndex(int xPos, int yPos, int rotation){
+int RotatedIndex(int pieceX, int pieceY, int rotation){
 	switch(rotation % 4){
-		case 0: return yPos * 4 + xPos;
-		case 1: return 12 + yPos - xPos * 4;
-		case 2: return 15 - yPos * 4 - xPos;
-		case 3: return 3 - yPos + xPos * 4;
+		case 0: return pieceY * 4 + pieceX;
+		case 1: return 12 + pieceY - pieceX * 4;
+		case 2: return 15 - pieceY * 4 - pieceX;
+		case 3: return 3 - pieceY + pieceX * 4;
 	}
 
 	return 0;
