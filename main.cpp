debe12691a7ef9b53d6a04e914c7019ce86dd2b3
mpdacey <80190692+mpdacey@users.noreply.github.com>
2024-01-05 17:21:18 +1100

    Added board

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 using namespace std;
 
 wstring tetromino[7];
+int boardWidth = 10;
+int boardHeight = 20;
+unsigned char *board = nullptr;
 
 int RotatedIndex(int pieceX, int pieceY, int rotation){
 	switch(rotation % 4){
@@ -14,6 +17,16 @@ int RotatedIndex(int pieceX, int pieceY, int rotation){
 	return 0;
 }
 
+void InitialiseBoard(){
+	board = new unsigned char[(boardWidth+2) * (boardHeight+1)];
+	for (int x = 0; x < boardWidth+2; x++)
+		for (int y = 0; y < boardHeight+1; y++)
+			board[y*boardWidth + x] = (x == 0 || x == boardWidth + 1 || y == boardHeight) ? 9 : 0;
+}
+
 int main() {
+
+	InitialiseBoard();
+
 	return 0;
 }
