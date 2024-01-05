bde46742fab78f04b5f5c98cd462f7bf39f2d180
mpdacey <80190692+mpdacey@users.noreply.github.com>
2024-01-05 15:05:25 +1100

    Implemented index rotation

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 using namespace std;
 
+wstring tetromino[7];
+
+int RotatedIndex(int xPos, int yPos, int rotation){
+	switch(rotation % 4){
+		case 0: return yPos * 4 + xPos;
+		case 1: return 12 + yPos - xPos * 4;
+		case 2: return 15 - yPos * 4 - xPos;
+		case 3: return 3 - yPos + xPos * 4;
+	}
+
+	return 0;
+}
+
 int main() {
 	return 0;
 }
