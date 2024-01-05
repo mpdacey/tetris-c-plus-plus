8edc7902fa415f628b2ebf1401a12984b1d477f7
mpdacey <80190692+mpdacey@users.noreply.github.com>
2024-01-05 19:40:58 +1100

    Board now renders in cmd

--- main.cpp
+++ main.cpp
@@ -8,8 +8,10 @@ int boardWidth = 10;
 int boardHeight = 20;
 unsigned char *board = nullptr;
 
-int screenWidth = 80;
-int screenHeight = 30;
+int screenWidth = 120;
+int screenHeight = 60;
+int boardOffsetX = 2;
+int boardOffsetY = 2;
 
 int RotatedIndex(int pieceX, int pieceY, int rotation){
 	switch(rotation % 4){
@@ -23,23 +25,34 @@ int RotatedIndex(int pieceX, int pieceY, int rotation){
 }
 
 void InitialiseBoard(){
-	board = new unsigned char[(boardWidth+2) * (boardHeight+1)];
-	for (int x = 0; x < boardWidth+2; x++)
-		for (int y = 0; y < boardHeight+1; y++)
-			board[y*boardWidth + x] = (x == 0 || x == boardWidth + 1 || y == boardHeight) ? 9 : 0;
+	boardWidth += 2;
+	boardHeight += 1;
+
+	board = new unsigned char[(boardWidth) * (boardHeight)];
+	for (int x = 0; x < boardWidth; x++)
+		for (int y = 0; y < boardHeight; y++)
+			board[y*boardWidth + x] = (x == 0 || x == boardWidth -1 || y == boardHeight - 1) ? 9 : 0;
 }
 
 int main() {
 
 	InitialiseBoard();
 
-	wchar_t *screen = new wchar_t[screenWidth*screenHeight];
+	wchar_t* screen = new wchar_t[screenWidth*screenHeight];
 	for (int i = 0; i < screenWidth*screenHeight; i++) screen[i] = L' ';
 	HANDLE console = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
 	SetConsoleActiveScreenBuffer(console);
 	DWORD dwBytesWritten = 0;
 
-	WriteConsoleOutputCharacter(console, screen, screenWidth * screenHeight, {0,0}, &dwBytesWritten);
+	bool gameOverFlag = false;
+
+	while(!gameOverFlag){
+		for (int x = 0; x < boardWidth; x++)
+			for (int y = 0; y < boardHeight; y++)
+				screen[(y + boardOffsetY) * screenWidth + x + boardOffsetX] = L" ABCDEFG=#"[board[y * boardWidth+x]];
+
+		WriteConsoleOutputCharacter(console, screen, screenWidth * screenHeight, { 0,0 }, &dwBytesWritten);
+	}
 
 	return 0;
 }
